7767ddeb249abaed8a0fc34acadb6217a18a2c1c
Ana-Kaleb <Ana-Kaleb@test.com>
2022-03-14 10:49:43 -0300

    Exercicios de 14/03/2022

---
+++ src/ExercicioUm.java
@@ -0,0 +1,9 @@
+
+public class ExercicioUm {
+public static void main(String[] args) {
+	String nome = "Ana ", sobrenome = "Carolina de Almeida", resultado = nome + sobrenome;
+	int idade = 16;
+	System.out.println(""+ nome +sobrenome +" tem " + idade + " anos");
+	System.out.println(""+resultado);
+}
+}
---
+++ src/PrimeiroExemplo.java
@@ -0,0 +1,10 @@
+
+public class PrimeiroExemplo {
+	
+public static void main(String[] args) {
+	String nome = "José";
+	int idade = 65;
+	System.out.println("Seu nome é: " +nome);
+	System.out.println("Sua idade é: " +idade);
+}
+}
