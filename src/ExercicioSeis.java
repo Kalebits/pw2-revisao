849fa4e93af67ea6eebaf05376a852b09e1e011b
Gabriel-Kaleb <gabriel@gmail.com>
2022-03-28 09:50:30 -0300

    Desenvolvimento do exercicio 7

--- src/ExercicioSeis.java
+++ src/ExercicioSeis.java
@@ -1,4 +1,14 @@
 
 public class ExercicioSeis {
+	public static void main(String[] args) {
+		double peso = 4;
 
+		if (peso < 4) {
+			System.out.println("Abaixo do peso");
+		} else if (peso > 10) {
+			System.out.println("Acima do peso");
+		} else {
+			System.out.println("Peso normal");
+		}
+	}
 }
