da8c6a3e1ba77508e6be4959fd9ce93e268ba790
Gabriel-Kaleb <gabriel@gmail.com>
2022-03-28 10:23:07 -0300

    Desenvolvimento do exercicio 8

---
+++ src/ExercicioOito.java
@@ -0,0 +1,11 @@
+
+public class ExercicioOito {
+	public static void main(String[] args) {
+		String semana = "Sabado";
+		if (semana != "Sabado" && semana != "Domingo") {
+			System.out.println("Você pode ir ao banco");
+		} else {
+			System.out.println("O banco está fechado, tente outro dia");
+		}
+	}
+}
--- src/ExercicioSete.java
+++ src/ExercicioSete.java
@@ -1,21 +1,17 @@
 
 public class ExercicioSete {
-public static void main(String[] args) {
-	double l1 = 5;
-	double l2 = 5;
-	double l3 = 3;
-	
-	if(l1 == l2 && l1 == l3)
-	{
-		System.out.println("Equilátero");
-	}else if(l1 == l2 || l2 == l3 || l1 == l3)
-	{
-		System.out.println("Isósceles");
-	}else if(l1 + l2 > l3 && l1 + l3 > l2 && l2 + l3 > l1)
-	{
-		System.out.println("Escaleno");
+	public static void main(String[] args) {
+		double l1 = 5;
+		double l2 = 5;
+		double l3 = 3;
+
+		if (l1 == l2 && l1 == l3) {
+			System.out.println("Equilátero");
+		} else if (l1 == l2 || l2 == l3 || l1 == l3) {
+			System.out.println("Isósceles");
+		} else if (l1 + l2 > l3 && l1 + l3 > l2 && l2 + l3 > l1) {
+			System.out.println("Escaleno");
+		}
+
 	}
-		
-	
-}
 }
