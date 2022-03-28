4df0fda0c2f4481115a59a0940d8701c2da1feb3
Gabriel-Kaleb <gabriel@gmail.com>
2022-03-28 09:36:07 -0300

    Desenvolvimento do exercicio 5

---
+++ src/ExercicioCinco.java
@@ -0,0 +1,14 @@
+
+public class ExercicioCinco {
+public static void main(String[] args) {
+	double trabalhoDeHistoria = 8;
+	double trabalhoDeMatematica = 7;
+	double trabalhoDeCiencia = 10;
+	double trabalhoDeGeografia = 9.5;
+	double media = (trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia) / 4;
+	
+	System.out.println("A média é: " +media);
+	
+	
+}
+}
